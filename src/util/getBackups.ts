d616854ad3b18ae3e6f1f50ac5783e995940d750
piman51277 <piman.dev@gmail.com>
2024-01-20 01:04:29 -0500

    fix inconsistent sorting of backup options

--- src/util/getBackups.ts
+++ src/util/getBackups.ts
@@ -1,4 +1,4 @@
-import { readdirSync } from "fs";
+import { readdirSync, statSync } from "fs";
 import { config } from "../env/config.js";
 
 export function getBackups(): string[] {
@@ -6,7 +6,18 @@ export function getBackups(): string[] {
 
   return children
     .filter((child) => child.isFile())
+    .filter((child) => child.name.includes(".pmbk"))
     .map((child) => child.name)
-    .filter((name) => name.includes(".pmbk"))
-    .sort((a, b) => b.localeCompare(a));
+    .sort((a, b) => {
+      //pattern is name-YYYY-MM-DD_rel.pmbk
+      //it loosely follows the ISO 8601 date format, so Date() can parse it
+      const regex = /(\d{4}-\d{1,2}-\d{1,2})/;
+      const dateStringA = a.match(regex)![1];
+      const dateStringB = b.match(regex)![1];
+      if (!dateStringA || !dateStringB) return 0;
+
+      const dateA = new Date(dateStringA);
+      const dateB = new Date(dateStringB);
+      return dateA > dateB ? -1 : 1;
+    });
 }
